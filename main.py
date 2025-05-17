6c35272b6c2f1f46ea3c06b3d456ba91717761b6
sigitjr00702 <sigitjr00702@gmail.com>
2025-05-17 12:20:25 +0700

    perbaiki agar dapat diakases

--- main.py
+++ main.py
@@ -100,4 +100,6 @@ def disconnect():
     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     socketio.run(app, debug=True)
